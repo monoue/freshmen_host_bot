ab25d99eb5e5fe503c2dcdfb9dc9ffc4a28979c5
monoue <like_a_newborn_baby@icloud.com>
2021-07-13 12:37:58 +0900

    add: giving both roles entirely

--- cogs/role.py
+++ cogs/role.py
@@ -16,27 +16,25 @@ class Role(commands.Cog):
                 return True
         return False
     
-    def create_role(self, guild, name, color=None):
+    async def create_role(self, guild, name, color=None):
         if self.role_exists(guild, name):
             return
         if color == None:
-            guild.create_role(name=name)
+            await guild.create_role(name=name)
         else:
-            guild.create_role(name=name, colour=color)
+            await guild.create_role(name=name, colour=color)
     
-
     @commands.command()
     async def create_freshman_role(self, ctx):
         name = "freshman"
         color = 0xf172a3
-        create_role(ctx.guild, name, color)
+        await self.create_role(ctx.guild, name, color)
     
     @commands.command()
     async def create_core_time_role(self, ctx):
         name = "core_time"
-        create_role(ctx.guild, name)
+        await self.create_role(ctx.guild, name)
     
-
     def executed_by_monoue(self, author_id):
         return author_id == config.MONOUE_ID
 
@@ -53,22 +51,38 @@ class Role(commands.Cog):
     def role_is_already_given(self, role, given_roles):
         return role in given_roles
     
-    async def add_freshman_role_if_appropriate(self, member, freshman_role):
-        if self.role_is_already_given(freshman_role, member.roles):
+    def is_student(self, member_roles):
+        for role in member_roles:
+            if role.name == "student":
+                return True
+        return False
+
+    async def add_role_if_appropriate(self, member, role):
+        if self.role_is_already_given(role, member.roles):
             return
-        if member.joined_at > datetime.now() - relativedelta(months=1)
-            await member.add_roles(freshman_role)
+        if not self.is_student(member.roles):
+            return
+        if member.joined_at > datetime.now() - relativedelta(months=1):
+            await member.add_roles(role)
 
-
-    @commands.command()
-    async def add_freshman_role_entirely(self, ctx):
+    async def add_role_entirely(self, ctx, role_name):
         if not self.executed_by_priviledged_member(ctx):
             return
         guild = ctx.guild
         members = guild.members
-        freshman_role = discord.utils.get(guild.roles, name="freshman")
+        role = discord.utils.get(guild.roles, name=role_name)
         for member in members:
-            await self.add_freshman_role_if_appropriate(member, freshman_role)
+            await self.add_role_if_appropriate(member, role)
+
+    @commands.command()
+    async def add_core_time_role_entirely(self, ctx):
+        role_name = "core_time"
+        await self.add_role_entirely(ctx, role_name)
+
+    @commands.command()
+    async def add_freshman_role_entirely(self, ctx):
+        role_name = "freshman"
+        await self.add_role_entirely(ctx, role_name)
     
     # 全体用の remove も作っておく
 
